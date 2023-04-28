e2a72b136802a2c40fc92208712429c7e9c2e53a
Devatanu Banerjee <devatanu@gmail.com>
2023-04-28 10:49:33 -0400

    first iteration without html file

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+import yfinance as yf
+import matplotlib.pyplot as plt
+import mpld3
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Define the ticker symbol
+tickerSymbol = 'GOOGL'
 
+# Get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Get the historical prices for this ticker
+tickerDf = tickerData.history(period='50d')
 
+# Calculate the moving averages
+ma_9 = tickerDf['Close'].rolling(window=9).mean()
+ma_20 = tickerDf['Close'].rolling(window=20).mean()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Plot the closing prices and moving averages
+fig, ax = plt.subplots()
+ax.plot(tickerDf.index, tickerDf['Close'], label='Closing Price')
+ax.plot(tickerDf.index, ma_9, label='9-day MA')
+ax.plot(tickerDf.index, ma_20, label='20-day MA')
+ax.set_xlabel('Date')
+ax.set_ylabel('Price')
+ax.set_title('Google Stock Price')
+ax.legend()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Convert the plot to HTML
+html_fig = mpld3.fig_to_html(fig)
+
+# Write the HTML to a file
+with open('plot.html', 'w') as f:
+    f.write(html_fig)
